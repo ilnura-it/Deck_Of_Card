e4612a86b0879d97c9c1fc089f5b7ab50007667b
ilnura <t.ilnura@gmail.com>
2023-10-20 16:33:57 -0600

    changes in code

--- deck_of_card.py
+++ deck_of_card.py
@@ -1,3 +1,7 @@
+import random
+
+card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+card_suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 class Card:
 
    def __init__(self, suit, value):
@@ -5,18 +9,40 @@ class Card:
       self.value = value
 
    def __repr__(self):
-      return f"Card {self.suit} {self.value}"
+      return f"{self.value} of {self.suit}"
    
 class Deck:
 
-   def __init__(self, cards):
-      self.cards = cards
-
+   def __init__(self):
+      self.cards = []
+      self.cards = [Card(suit, value) for suit in card_suits for value in card_values]
+      
    def __repr__(self):
       return f"Deck of {self.cards} cards"
    
-card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+   def _deal(self, num):
+      if len(self.cards) == 0:
+         raise ValueError("All cards have been dealt")
+         
+      new_deck = self.cards[:-num]
+      return new_deck
+   
+   def count(self):
+      return len(self.cards)
+   
+   def shuffle(self):
+      if len(self.cards) == 52:
+         return random.shuffle(self.cards)
+      elif len(self.cards) < 53:
+         raise ValueError("Only full decks can be shuffled")
+   
+   def deal_card(self):
+      return self.cards.pop(0)
 
-card_suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+   def deal_hand(self, num):
+      hand = [self.cards.pop(0) for _ in range(num)]
+      return hand
 
-card = [val for val in card_values]
+my_deck = Deck()
+# print(my_deck)
+print(my_deck.deal_hand(5))
