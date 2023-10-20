efac5fd2b545adcd9d258c66dfb2ff8a1a8c6f43
ilnura <t.ilnura@gmail.com>
2023-10-20 15:18:05 -0600

    edited

--- deck_of_card.py
+++ deck_of_card.py
@@ -7,3 +7,16 @@ class Card:
    def __repr__(self):
       return f"Card {self.suit} {self.value}"
    
+class Deck:
+
+   def __init__(self, cards):
+      self.cards = cards
+
+   def __repr__(self):
+      return f"Deck of {self.cards} cards"
+   
+card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+card_suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+card = [val for val in card_values]
